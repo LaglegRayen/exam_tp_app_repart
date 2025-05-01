415e49d82f902d7fed77869addf58106563f8ec5
LaglegRayen <rayen.laglag@insat.ucar.tn>
2025-05-01 15:03:55 +0100

    added app

---
+++ src/main/java/com/example/inspirationalquotesapi/InspirationalQuotesApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.inspirationalquotesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspirationalQuotesApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InspirationalQuotesApiApplication.class, args);
+    }
+}
